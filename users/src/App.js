dfbb2b241344ed2cd59720d58981e31323563ac0
Ziyi Zhang <zhangziyibupt@gmail.com>
2022-11-10 11:26:52 -0800

    Complete the users app, add state for input/list/errormodal components

--- users/src/App.js
+++ users/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUsersList((prevState) => [
+      ...prevState,
+      { name: username, age: age, id: Math.random().toString() },
+    ]);
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
