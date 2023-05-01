f9128e1b7e279beb2c4fefcfcf551a77ceec6b42
Ziyi Zhang <zhangziyibupt@gmail.com>
2023-04-30 22:08:47 -0700

    Update images

--- meals/src/components/Layout/Header.js
+++ meals/src/components/Layout/Header.js
@@ -8,11 +8,11 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>React Meals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} />
+        <img src={mealsImage} alt="table of foods" />
       </div>
     </Fragment>
   );
