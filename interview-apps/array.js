96e14f6a6086061527a2a300cacc4a4e4b7b4a83
Ziyi Zhang <zhangziyibupt@gmail.com>
2023-09-20 01:02:43 -0700

    add js coding questions

--- interview-apps/array.js
+++ interview-apps/array.js
@@ -1,4 +1,4 @@
-// https://www.greatfrontend.com/questions/javascript/array-filter
+// filter()
 Array.prototype.myFilter = function (callbackFn, thisArg) {
   const len = this.length;
   const results = [];
@@ -11,3 +11,60 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
   }
   return results;
 };
+
+// reduce()
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  const noInitialValue = initialValue == undefined;
+  const len = this.length;
+  if (noInitialValue  && len === 0) {
+    throw new Error();
+  }
+  let result = noInitialValue ? this[0] : initialValue;
+  let startIndex = noInitialValue ? 1 : 0;
+
+  for (let k = startIndex; k < len; k++) {
+    // sparse array [1, 2, , 3], this[2] = NaN
+    // 0: 1, 1: 2, 3: 3
+    // if (Object.hasOwn(this, k)) {
+      result = callbackFn(result, this[k], k, this);
+    // }
+  }
+  return result;
+};
+
+// bind()
+Function.prototype.myBind = function (thisArg, ...boundArgs) {
+  const original = this;
+  return (...args) => {
+    return original.apply(thisArg, [...boundArgs, ...args]);
+  }
+};
+
+// flatten()
+// Single-level arrays are unaffected.
+flatten([1, 2, 3]); // [1, 2, 3]
+
+// Inner arrays are flattened into a single level.
+flatten([1, [2, 3]]); // [1, 2, 3]
+flatten([
+  [1, 2],
+  [3, 4],
+]); // [1, 2, 3, 4]
+
+// Flattens recursively.
+flatten([1, [2, [3, [4, [5]]]]]); // [1, 2, 3, 4, 5]
+
+function flatten(value) {
+  const res = [];
+  const copy = value.slice();
+
+  while (copy.length > 0) {
+    const item = copy.shift(); // remove the first element in the array, return the shifted element.
+    if (Array.isArray(item)) {
+      copy.unshift(...item); // add new elements to the begining of the array
+    } else {
+      res.push(item);
+    }
+  }
+  return res;
+}
--- interview-apps/lodash.js
+++ interview-apps/lodash.js
@@ -15,3 +15,58 @@ const get = (object, path, defaultValue) => {
   const value = index === length ? object : undefined;
   return value !== undefined ? value : defaultValue;
 }
+
+// debounce()
+function debounce(func, wait = 0) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args); // this in the returned function scope
+    }, wait); 
+  }
+}
+
+let i = 0;
+function increment() {i++}
+const debouncedIncrement = debounce(increment, 100);
+
+// t = 0: Call debouncedIncrement().
+debouncedIncrement(); // i = 0
+
+// t = 50: i is still 0 because 100ms have not passed.
+//  Call debouncedIncrement() again.
+debouncedIncrement(); // i = 0
+
+// t = 100: i is still 0 because it has only
+//  been 50ms since the last debouncedIncrement() at t = 50.
+
+// t = 150: Because 100ms have passed since
+//  the last debouncedIncrement() at t = 50,
+//  increment was invoked and i is now 1
+
+// throttle()
+function throttle(func, wait) {
+  let shouldThrottle = false;
+
+  return function(...args) {
+    if (shouldThrottle) return;
+
+    shouldThrottle = true;
+    setTimeout(() => {shouldThrottle = false}, wait);
+    func.apply(this, args);
+  }
+}
+
+const throttledIncrement = throttle(increment, 100);
+// t = 0: Call throttledIncrement(). i is now 1.
+throttledIncrement(); // i = 1
+
+// t = 50: Call throttledIncrement() again.
+// i is still 1 because 100ms have not passed.
+throttledIncrement(); // i = 1
+
+// t = 101: Call throttledIncrement() again. i is now 2.
+// i can be incremented because it has been more than 100ms
+// since the last throttledIncrement() call at t = 0.
+throttledIncrement(); // i = 2
